b1fec7d998e7524eb28d3ce31268d1a2179e6d1b
Dinesh Umasankar <dinesh71uma@gmail.com>
2025-12-14 14:48:29 -0500

    feat: Add mini.icons support for blink completions

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -259,6 +259,24 @@ later(function()
         },
         signature = { enabled = true },
         fuzzy = { implementation = "prefer_rust_with_warning" },
+        completion = {
+            menu = {
+                draw = {
+                    components = {
+                        kind_icon = {
+                            text = function(ctx)
+                                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                                return kind_icon
+                            end,
+                            highlight = function(ctx)
+                                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                                return hl
+                            end,
+                        }
+                    }
+                }
+            }
+        },
     })
 end)
 
